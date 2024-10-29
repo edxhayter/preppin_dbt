b31e129f66a31f9059873183fd68543adf71beed
Edward Hayter <egkhayter@gmail.com>
2024-10-29 11:50:14 +0000

    Staging done - all that is left is to flatten the music column and split song and artist out in the flattened rows

---
+++ models/2024wk42/int_2024wk42__flatten.sql
@@ -0,0 +1,65 @@
+-- bring in the staged model
+
+WITH stage AS (
+
+    SELECT * FROM {{ ref('stg_2024wk42__input') }}
+
+),
+
+-- split out solo dances and joint dances, remove duplicate rows from joint dances and union back on
+solos AS (
+
+    SELECT
+
+        year,
+        series,
+        week,
+        stage,
+        theme,
+        theme_detail,
+        couple,
+        dance,
+        music,
+        result,
+        overall_score,
+        judges_scores
+
+    FROM stage
+
+    WHERE contains(dance, 'Group') = FALSE OR contains(dance, 'Marathon') = FALSE -- AND week = '12 (Semi-final)'
+
+),
+
+joint AS (
+
+    SELECT
+
+        year,
+        series,
+        week,
+        stage,
+        theme,
+        theme_detail,
+        'Group' AS couple,
+        dance,
+        music,
+        result,
+        NULL AS overall_score,
+        judges_scores
+
+    FROM stage
+    WHERE contains(dance, 'Group') OR contains(dance, 'Marathon')
+    GROUP BY ALL
+
+),
+
+suppressed AS (
+
+    SELECT * FROM solos
+    UNION ALL
+    SELECT * FROM joint
+)
+
+-- flatten out the song column when a dance has multiple songs so that there is a row for each song
+
+SELECT * FROM suppressed
